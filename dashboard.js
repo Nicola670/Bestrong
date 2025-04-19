5417cd825a72e6abe80545ca8cee94ac15b0d8a8
s02952 <s8814972y@fermibassano.edu.it>
2025-04-19 16:53:20 +0200

    feat: Definito elementi del DOM e predisposto le funzioni da implementare

---
+++ dashboard.js
@@ -0,0 +1,50 @@
+//elementi del DOM
+const clientsGrid = document.getElementById('clientsGrid');
+const addClientBtn = document.getElementById('addClientBtn');
+const addClientModal = document.getElementById('addClientModal');
+const closeAddModal = document.getElementById('closeAddModal');
+const cancelAddClient = document.getElementById('cancelAddClient');
+const addClientForm = document.getElementById('addClientForm');
+const clientDetailsModal = document.getElementById('clientDetailsModal');
+const closeDetailsModal = document.getElementById('closeDetailsModal');
+const searchInput = document.getElementById('searchInput');
+
+//funzione per ottenere le iniziali di nome e cognome
+function getInitials(nome, cognome){
+
+}
+
+//funzione per creare la card dell'utente
+function createClientCard(client){
+
+}
+
+//funzione per formattare l'obbiettivo
+function formatObjective(objective){
+
+}
+
+//funzione per calcolare l'eta
+function calculateAge(birthDate){
+
+}
+
+//funzione per formattare la data
+function formatDate(dateString){
+
+}
+
+//funzione per mostrare i dettagli di un cliente
+function showClientDetail(client){
+
+}
+
+// Funzione per impostare le tab
+function setupTabs(){
+
+}
+
+// Funzione per caricare i clienti
+function loadClients(clientsData){
+    
+}
