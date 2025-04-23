1c9ca9cd55db4e5a91bd578a9dc821d472052acd
s02713 <s02713@fermi.intra>
2025-04-23 09:23:30 +0200

    feat: creata base flask

--- server.py
+++ server.py
@@ -1,2 +1,13 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
 
 
+@app.route('/')
+def home():
+    appName = "BeStrong"
+    return render_template("home.html", appName = appName)
+
+if __name__ == "__main__":
+    app.run()
