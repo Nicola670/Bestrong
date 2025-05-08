bd1dca59c60ecc5c676f2e538ad9871b9947167f
s02834 <s8815029v@fermibassano.edu.it>
2025-05-08 17:23:31 +0200

    aggiunta rotta per gestire la pagina 404

--- server.py
+++ server.py
@@ -218,6 +218,10 @@ def profile():
     
     return render_template('profilo.html')
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
 if __name__ == "__main__":
     # Inizializza e popola il database
     initialize_db()
